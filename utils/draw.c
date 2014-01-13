678b84e79c3811da4a0f0bd80d36c16808247a62
Erik <err-g@users.noreply.github.com>
2014-01-13 22:41:28 +0000

    some changes

--- utils/draw.c
+++ utils/draw.c
@@ -6,14 +6,14 @@
 /*   By: ecarvalh <ecarvalh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 17:27:46 by ecarvalh          #+#    #+#             */
-/*   Updated: 2024/03/03 00:53:27 by ecarvalh         ###   ########.fr       */
+/*   Updated: 2024/03/03 01:16:50 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	draw_line(t_fdf *fdf, int line[4], int color);
-void	draw_shape(t_fdf *fdf, t_vec3 **points, int **edges);
+void	draw_line(t_fdf *fdf, int line[2][2], int color);
+void	draw_shape(t_fdf *fdf, int **points, int **edges);
 
 /* [Thnks Wikipedia!](https://en.wikipedia.org/wiki/Bresenham's_line_algorithm)
  *
@@ -21,47 +21,47 @@ void	draw_shape(t_fdf *fdf, t_vec3 **points, int **edges);
  * of drawing lines, I can draw triangles. You can make any shape with only
  * triangles.
  */
-void	draw_line(t_fdf *fdf, int line[4], int color)
+void	draw_line(t_fdf *fdf, int line[2][2], int color)
 {
-	t_vec2	dif;
-	t_vec2	dir;
-	int		err;
+	int	*dif;
+	int	*dir;
+	int	err;
 
-	dif = (t_vec2){
-		abs(line[2] - line[0]),
-		-abs(line[3] - line[1])};
-	dir = (t_vec2){
-		_tern(line[0] < line[2], 1, -1),
-		_tern(line[1] < line[3], 1, -1)};
-	err = dif.x + dif.y;
-	while (line[0] != line[2] || line[1] != line[3])
+	dif = (int [2]){
+		abs(line[1][X] - line[0][X]),
+		-abs(line[1][Y] - line[0][Y])};
+	dir = (int [2]){
+		_tern(line[0][X] < line[1][X], 1, -1),
+		_tern(line[0][Y] < line[1][Y], 1, -1)};
+	err = dif[X] + dif[Y];
+	while (line[0][X] != line[1][X] || line[0][Y] != line[1][Y])
 	{
-		mlx_pixel_put(fdf->mlx, fdf->win, line[0], line[1], color);
-		if (2 * err >= dif.y && line[0] != line[2])
+		mlx_pixel_put(fdf->mlx, fdf->win, line[0][X], line[0][Y], color);
+		if (2 * err >= dif[Y] && line[0][X] != line[1][X])
 		{
-			err += dif.y;
-			line[0] += dir.x;
+			err += dif[Y];
+			line[0][X] += dir[X];
 		}
-		if (2 * err <= dif.x && line[1] != line[3])
+		if (2 * err <= dif[X] && line[0][Y] != line[1][Y])
 		{
-			err += dif.x;
-			line[1] += dir.y;
+			err += dif[X];
+			line[0][Y] += dir[Y];
 		}
 	}
 }
 
-void	draw_shape(t_fdf *fdf, t_vec3 **points, int **edges)
+void	draw_shape(t_fdf *fdf, int **points, int **edges)
 {
 	int	i;
-	int	line[4];
+	int	line[2][2];
 
 	i = -1;
 	while (edges[++i])
 	{
-		line[0] = points[edges[i][0]]->x;
-		line[1] = points[edges[i][0]]->y;
-		line[2] = points[edges[i][1]]->x;
-		line[3] = points[edges[i][1]]->y;
+		line[0][X] = points[edges[i][0]][X];
+		line[0][Y] = points[edges[i][0]][Y];
+		line[1][X] = points[edges[i][1]][X];
+		line[1][Y] = points[edges[i][1]][Y];
 		draw_line(fdf, line, 0xffffff);
 	}
 }
--- utils/fdf.c
+++ utils/fdf.c
@@ -6,7 +6,7 @@
 /*   By: ecarvalh <ecarvalh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 17:29:39 by ecarvalh          #+#    #+#             */
-/*   Updated: 2024/03/03 00:55:18 by ecarvalh         ###   ########.fr       */
+/*   Updated: 2024/03/03 01:21:56 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	fdf_init(t_fdf *fdf);
 
 int	fdf_draw(t_fdf *fdf)
 {
-	t_vec3	**points;
-	int		**edges;
+	int	**points;
+	int	**edges;
 
-	points = (t_vec3 *[]){
-		&(t_vec3){0, 0, 0}, &(t_vec3){100, 0, 0}, &(t_vec3){100, 100, 0},
-		&(t_vec3){0, 100, 0}, &(t_vec3){0, 0, 100}, &(t_vec3){100, 0, 100},
-		&(t_vec3){100, 100, 100}, &(t_vec3){0, 100, 100}, NULL};
+	points = (int *[]){
+		(int [3]){0, 0, 0}, (int [3]){100, 0, 0}, (int [3]){100, 100, 0},
+		(int [3]){0, 100, 0}, (int [3]){0, 0, 100}, (int [3]){100, 0, 100},
+		(int [3]){100, 100, 100}, (int [3]){0, 100, 100}, NULL};
 	edges = (int *[]){
 		(int [2]){0, 1}, (int [2]){1, 2}, (int [2]){2, 3}, (int [2]){3, 0},
 		(int [2]){4, 5}, (int [2]){5, 6}, (int [2]){6, 7}, (int [2]){7, 4},
@@ -39,9 +39,10 @@ void	fdf_init(t_fdf *fdf)
 	fdf->mlx = mlx_init();
 	if (!fdf->mlx)
 		exit(1);
-	fdf->win_size = (t_vec2){300, 300};
+	fdf->win_size[X] = 300;
+	fdf->win_size[Y] = 300;
 	fdf->win = mlx_new_window(
-			fdf->mlx, fdf->win_size.x, fdf->win_size.y, "fdf");
+			fdf->mlx, fdf->win_size[X], fdf->win_size[Y], "fdf");
 	if (!fdf->win)
 	{
 		free(fdf->mlx);
